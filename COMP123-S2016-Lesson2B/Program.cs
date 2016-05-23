6ca8ab0528c07f00ecf6bb1bfba03c91afa8049d
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-05-23 14:13:20 -0400

    added readUnitEndMethod and refactored codebase

---
+++ COMP123-S2016-Lesson2B/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Lesson2B
+{
+    /**
+     * Main driver class for Lesson2 
+     * 
+     * @class Program
+     * 
+     */
+    public class Program
+    {
+        /**
+         * Main Method for our drive class
+         * 
+         * @constructor Main
+         * @pram {string[]} args
+         */
+        static void Main(string[] args)
+        {
+            int x = 50;
+            int y = 40;
+            int result = 0;
+            int numberOfEntries = 0;
+
+            result = addXandY(x, y);
+            Console.WriteLine(result);
+            Console.WriteLine();
+
+            addYtoX(ref x, ref y);//pass 50 = +40
+            Console.WriteLine(x);
+            Console.WriteLine();
+
+          
+
+         
+              Console.WriteLine("How many entries do you want to make");
+              int.TryParse(Console.ReadLine(), out numberOfEntries);
+             
+              if(numberOfEntries > 0)
+              {
+                  readUntilEnd(numberOfEntries);
+              }
+        
+         
+        }
+        
+        /**
+         * simple Method to add two values
+         * 
+         * @method addXandY
+         * @param {int} firstNumber
+         * @param {int} secondNumber
+         */
+        public static int addXandY(int firstNumber, int SecondNumber)
+        {
+            int result = 0;
+            result = firstNumber + SecondNumber;
+            return result;
+
+
+
+        }
+
+        /**
+         * simple method to demonstrate pass by reference with ref Keyword
+         * 
+         * @method addYtoX
+         * @param {ref int} X
+         * @param {ref int} Y
+         * 
+         */
+        public static int addYtoX(ref int X, ref int Y)
+        {
+            X += Y;
+            return X;
+        }
+        public static int readUntilEnd(int numberOfEntries)
+        {
+          
+           
+            string[] inputs = new string[numberOfEntries];
+            int inputCounter=0;
+
+
+            do
+            {
+                Console.WriteLine("Enter a value -('end' to stop): ");
+                inputs[inputCounter] = "";
+                inputs[inputCounter] = Console.ReadLine();
+                inputCounter++;
+               
+            } while ((inputs[inputCounter-1] != "end")&&(inputCounter<numberOfEntries));
+
+            return inputCounter;
+        }
+    }
+}
---
+++ UnitTestForLesson2B/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMP123_S2016_Lesson2B;
+
+namespace UnitTestForLesson2B
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void addXandYTestMethod()
+        {
+            //Arrange
+            int x = 40;
+            int y = 50;
+            int expectedResult = 90;
+
+            //Act
+
+            int result = Program.addXandY(x, y);
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void addYtoXTestMethod()
+        {
+            //Arrange
+            int x = 40;
+            int y = 50;
+            int expectedResult = 90;
+
+            //Act
+
+            int result = Program.addYtoX(ref x, ref y);
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void readUntilEndTestMethod()
+        {
+            //Arrange
+            int numberOfEnteries = 5;
+            int expectedCount = 5;
+            int actualCount = 0;
+            //Act
+            actualCount = Program.readUntilEnd(numberOfEnteries);
+            
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    }
+}
